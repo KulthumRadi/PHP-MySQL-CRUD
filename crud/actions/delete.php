3661e697dce81f703ce2b89032fe2e2d01766c92
KulthumRadi <kulthumradi94@gmail.com>
2022-01-18 12:56:26 +0200

    php delete and edit done

---
+++ crud/actions/delete.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../connection/connection.php');
+$connection = OpenConnection();
+if(isset($_GET['delete'])){
+    $id = $_GET['id'];
+$connection->query("DELETE FORM todo WHERE id=$id")or die($mysqli->error());
+}
+$connection->close()
+?>
---
+++ crud/actions/edit.php
@@ -0,0 +1,10 @@
+<?php
+require_once('../connection/connection.php');
+$connection = OpenConnection();
+if(isset($_POST['edit'])){
+    $id = $_POST['id'];
+    $title = $_POST['titee'];
+$connection->query("UPDATE todo SET  title = '$title' WHERE id = $id")or die($mysqli->error());
+}
+$connection->close()
+?>
